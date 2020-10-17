8efc4fd596889ce87b43e5f9a1a16dca3897c8ad
Diana <mdiannna@yahoo.com>
2020-10-17 16:00:55 +0300

    first working requests GET /nota-examen and POST /nota-examen

--- Services-Sanic/models.py
+++ Services-Sanic/models.py
@@ -1,7 +1,27 @@
 from sanic_motor import BaseModel
 
 # TODO: add necessary fields
-class Student(BaseModel):
-    __coll__ = 'students'
-    __unique_fields__ = ['name']
-    # __unique_fields__ = ['name, age']   # name and age for unique
+# class Student(BaseModel):
+#     __coll__ = 'students'
+#     __unique_fields__ = ['name']
+#     # __unique_fields__ = ['name, age']   # name and age for unique
+
+
+# {
+# 	"id" : "1",
+# 	"student": "NumePrenume",
+# 	"mark": 10,
+# 	"status": "processing"
+# },
+# nu stiu daca trebuie id
+class ExamMark(BaseModel):
+    __coll__ = 'exam_marks'
+    # __unique_fields__ = ['name']
+
+
+class MidtermMark(BaseModel):
+    __coll__ = 'midterm_marks'
+
+
+
+	
--- Services-Sanic/service_type2.py
+++ Services-Sanic/service_type2.py
@@ -16,7 +16,9 @@ from sanic.response import json
 from sanic import response
 from sanic_jinja2 import SanicJinja2
 from sanic_motor import BaseModel
-from models import Student
+from models import ExamMark, MidtermMark
+from controllers import MarksController
+from termcolor import colored
 
 app = Sanic(__name__)
 
@@ -41,39 +43,92 @@ jinja = SanicJinja2(app, autoescape=True)
 
 
 
-
 @app.route('/')
 async def index(request):
     return response.json({"server status": "up and running", "message":"Hello!"})
 
+# # Added new route
+# @app.route("/init-student", methods=["POST", "GET"])
+# async def init_student(request):
+#   if request.method=='POST':
+#       student = request.json.get("student", "")
+#       st = StudentController()
+#       return st.init_student(student)
+
 
 @app.route("/nota-examen", methods=["POST", "GET"])
-async def post_nota_examen(request):
-	pass
-	# TODO
-	return response.json({"status": "TODO"})
+async def nota_examen(request):
+    
+    # curl -d '{"student":"Diana Marusic", "nota": "10"}' -H 'Content-Type: application/json' http://127.0.0.1:8000/nota-examen
+    if request.method=='POST':
+        student = request.json.get("student", "")
+        nota = request.json.get("nota", "")
+        # TODO
+        await ExamMark.insert_one(dict(student=student, mark=int(nota), status="processing"))
+
+        return response.json({"status": "success", "message": "Exam mark saved successfully"})
+    
+        # return m.save_exam_mark(student, nota)
+
+    else: #GET
+        """
+        curl -X GET \
+      -H "content-type: application/json" \
+      -H "Accept: application/json" \
+      -d '{"student":"Diana Marusic"}' \
+      "http://127.0.0.1:8000/nota-examen"
+      """
+
+        print(colored("request:", "blue"))
+        print(request)
+        print(colored("request json:", "blue"))
+        print(request.json)
+        print(colored("-------:", "blue"))
+
+        student = request.json.get("student", "")        
+
+        # exam_mark = await ExamMark.find_one({"student":student}, as_raw=True)
+        exam_mark = await ExamMark.find_one({"student":student})
+        print(colored("exam mark:", "yellow"), exam_mark)   
+
+        print(colored("exam mark:", "yellow"), exam_mark.mark)   
+        if exam_mark:
+            return response.json({"status": "success", "student":student, "exam_mark": exam_mark.mark})
+
+        return response.json({"status": "error", "message": "Exam mark not found for student " + student + ". Error"})
+        
+    return response.json({"status": "TODO"})
 
 
 # curl -d '{"name":"Diana Marusic", "age": "22"}' -H 'Content-Type: application/json' http://127.0.0.1:8000/new
 @app.route("/pune-nota-atestare", methods=["POST"])
 async def pune_nota_atestart(request):
-	pass
-	# TODO
-	return response.json({"status": "TODO"})
+    if request.method=="POST":
+        student = request.json.get("student", "")
+        nota = request.json.get("nota", "")
+        nr_atestare = request.json.get("nr_atestare", "")
+
+    # TODO
+    return response.json({"status": "TODO"})
 
 
 @app.route("/nota-finala", methods=["POST"])
 async def get_nota_finala(request):
-	pass
-	# TODO
-	return response.json({"status": "TODO"})
+    if request.method=="POST":
+        student = request.json.get("student", "")
+    
+
+    # TODO
+    # return {“student”: “NumePrenume1”, “note_atestari”: [“atest1”: nota, “atest2”: “”] “nota_examen” : nota_examen ,“nota_finala”: 9.88} (daca o atestare lipseste, sau examenul, la nota finala ii returneaza eroare)
+    return response.json({"status": "TODO"})
 
 
 @app.route("/status", methods=["GET"])
 async def status(request):
-	pass
-	# TODO
-	return response.json({"status": "TODO"})
+    student = request.json.get("student", "")
+    
+    # TODO
+    return response.json({"status": "TODO"})
 
 
 
